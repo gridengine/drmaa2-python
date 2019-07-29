918ff343a32892933a7d4f309979d5bf9d05f4b4
Andre Alefeld <aalefeld@univa.com>
2019-07-29 14:00:34 +0200

    added test_failed.py test to verify failed attribute, minor corrections to Makefile

---
+++ test/test_failed.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#___INFO__MARK_BEGIN__
+########################################################################## 
+# Copyright 2016-2019 Univa Corporation
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0 
+# 
+# Unless required by applicable law or agreed to in writing, software 
+# distributed under the License is distributed on an "AS IS" BASIS, 
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+# See the License for the specific language governing permissions and 
+# limitations under the License. 
+########################################################################### 
+#___INFO__MARK_END__
+
+from .utils import generate_random_string
+from drmaa2 import JobSession
+from drmaa2 import JobState
+
+def test_get_info():
+    session_name = generate_random_string()
+    js = JobSession(session_name)
+    job_name = 'a-%s' % generate_random_string()
+    d = {'remote_command' : '/bin/sleep', 'args' : ['10'], 'job_name' : job_name}
+    j = js.run_job(d)
+    ji = j.get_info()
+    assert(ji.job_name == job_name)
+    print('\nGet info: %s' % (ji))
+
+def test_wait_terminated():
+    session_name = generate_random_string()
+    js = JobSession(session_name)
+    j = js.run_job({'remote_command' : '/bin/sleep', 'args' : ['10']})
+    j.wait_terminated()
+    s,ss = j.get_state()
+    assert(s == JobState.DONE)
+    print('\nWait terminated for job: %s' % (j))
+    ji = j.get_info()
+    print('\nGet info: %s' % (ji))
+
