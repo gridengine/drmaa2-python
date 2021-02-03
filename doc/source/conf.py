e7d7140976181c91159d454e9ce202298641f4ad
Andre Alefeld <aalefeld@univa.com>
2021-02-03 09:22:55 +0100

    fix for GE-8471 new copyright notices für AGE bundled Univa open source code at Altair

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
-#___INFO__MARK_BEGIN__
-########################################################################## 
-# Copyright 2016-2019 Univa Corporation
-# 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
+# ___INFO__MARK_BEGIN__
+#######################################################################################
+# Copyright 2008-2021 Univa Corporation (acquired and owned by Altair Engineering Inc.)
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License.
+#
 # You may obtain a copy of the License at
-# 
-#     http://www.apache.org/licenses/LICENSE-2.0 
-# 
-# Unless required by applicable law or agreed to in writing, software 
-# distributed under the License is distributed on an "AS IS" BASIS, 
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-# See the License for the specific language governing permissions and 
-# limitations under the License. 
-########################################################################### 
-#___INFO__MARK_END__
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#######################################################################################
+# ___INFO__MARK_END__
 #
 # UGE DRMAA2 Python API documentation build configuration file, created by
 # sphinx-quickstart on Thu Nov 22 00:58:43 2018.
