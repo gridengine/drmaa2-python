f54de50be5807b7db92e91af6ce2ae6020f54008
Max Weiss <mweiss@altair.com>
2023-06-28 14:31:07 +0200

    Set version to 8.8.0

--- util/params.mk
+++ util/params.mk
@@ -39,6 +39,6 @@ export DRMAA2_REL_STR    = Development
 ifneq ($(strip $(GIT_REV)),)
 export DRMAA2_REL_STR       = Development version ($(GIT_REV))
 endif
-export VERSION              = 8.8.0a0
+export VERSION              = 8.8.0
 export DRMAA2_PACKAGE_NAME  = drmaa2
 
